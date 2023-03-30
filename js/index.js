4ddf6f4807c9a550999cff01cf16a1ff85af232f
fossilfriend <egreenfest@gmail.com>
2023-03-30 15:08:55 +0000

    modify index.js to include track rendering

--- js/index.js
+++ js/index.js
@@ -1,42 +1,62 @@
 // Defines the top-level API for the igv module
 
-import MenuUtils from "./ui/menuUtils.js"
-import DataRangeDialog from "./ui/dataRangeDialog.js"
-import IGVGraphics from "./igv-canvas.js"
-import {createBrowser, createTrack, removeAllBrowsers, removeBrowser, visibilityChange} from './igv-create.js'
-import {doAutoscale} from "./util/igvUtils.js"
-import embedCss from "./embedCss.js"
-import version from "./version.js"
-import TrackView from "./trackView.js"
-import {igvxhr, oauth} from "../node_modules/igv-utils/src/index.js"
+import MenuUtils from "./ui/menuUtils.js";
+import DataRangeDialog from "./ui/dataRangeDialog.js";
+import IGVGraphics from "./igv-canvas.js";
+import {
+  createBrowser,
+  createTrack,
+  removeAllBrowsers,
+  removeBrowser,
+  visibilityChange,
+} from "./igv-create.js";
+import { doAutoscale } from "./util/igvUtils.js";
+import embedCss from "./embedCss.js";
+import version from "./version.js";
+import TrackView from "./trackView.js";
+import {
+  igvxhr,
+  oauth,
+  IGVMath,
+  FeatureCache,
+} from "../node_modules/igv-utils/src/index.js";
 
-const setApiKey = igvxhr.setApiKey
+import GenomicInterval from "./genome/genomicInterval";
+import featurePacker from "./feature/featurePacker";
+import TrackBase from "../js/trackBase.js";
 
-embedCss()
+const setApiKey = igvxhr.setApiKey;
+
+embedCss();
 
 function setGoogleOauthToken(accessToken) {
-    return oauth.setToken(accessToken)
+  return oauth.setToken(accessToken);
 }
 
 function setOauthToken(accessToken, host) {
-    return oauth.setToken(accessToken, host)
+  return oauth.setToken(accessToken, host);
 }
 
 export default {
-    IGVGraphics,
-    MenuUtils,
-    DataRangeDialog,
-    createTrack,
-    createBrowser,
-    removeBrowser,
-    removeAllBrowsers,
-    visibilityChange,
-    setGoogleOauthToken,
-    setOauthToken,
-    oauth,
-    version,
-    setApiKey,
-    doAutoscale,
-    TrackView
-}
-
+  IGVGraphics,
+  MenuUtils,
+  DataRangeDialog,
+  createTrack,
+  createBrowser,
+  removeBrowser,
+  removeAllBrowsers,
+  visibilityChange,
+  setGoogleOauthToken,
+  setOauthToken,
+  oauth,
+  version,
+  setApiKey,
+  doAutoscale,
+  TrackView,
+  featurePacker,
+  igvxhr,
+  FeatureCache,
+  IGVMath,
+  GenomicInterval,
+  TrackBase
+};
