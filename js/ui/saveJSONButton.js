7e46d3096651c40b92182a67dda1e14648dd52cb
Simon Cole <simonccole1@gmail.com>
2023-06-20 17:08:14 -0400

    Save JSON button is now functional

--- js/ui/saveJSONButton.js
+++ js/ui/saveJSONButton.js
@@ -1,11 +1,19 @@
 import {DOMUtils} from '../../node_modules/igv-utils/src/index.js'
+import { FileUtils } from '../../node_modules/igv-utils/src/index.js'
 
 const SaveJSONButton = function (parent, browser) {
     const button = DOMUtils.div({class: 'igv-navbar-button'})
     parent.append(button)
 
     button.textContent = 'Save JSON'
-    button.addEventListener('click', () => browser.toJSON());
+    button.addEventListener('click', () => downloadJSONFile(browser.toJSON()));
+}
+
+const downloadJSONFile = (json) => {
+    const path = 'igvjs.json'
+    const dataStr = JSON.stringify(json)
+    const data = URL.createObjectURL(new Blob([dataStr], {type: "application/octet-stream"}))
+    FileUtils.download(path, data)
 }
 
 export default SaveJSONButton
