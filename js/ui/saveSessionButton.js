15141b013af0094189cae38cf9f3480f3208962e
fossilfriend <egreenfest@gmail.com>
2023-07-10 09:30:27 -0400

    new location of domutils

--- js/ui/saveSessionButton.js
+++ js/ui/saveSessionButton.js
@@ -1,19 +1,21 @@
-import {DOMUtils} from '../../node_modules/igv-utils/src/index.js'
-import { FileUtils } from '../../node_modules/igv-utils/src/index.js'
+import { DOMUtils } from "../node_modules/igv-ui/dist/igv-ui.js";
+import { FileUtils } from "../../node_modules/igv-utils/src/index.js";
 
 const SaveSessionButton = function (parent, browser) {
-    const button = DOMUtils.div({class: 'igv-navbar-button'})
-    parent.append(button)
+  const button = DOMUtils.div({ class: "igv-navbar-button" });
+  parent.append(button);
 
-    button.textContent = 'Save Session'
-    button.addEventListener('click', () => downloadJSONFile(browser.toJSON()));
-}
+  button.textContent = "Save Session";
+  button.addEventListener("click", () => downloadJSONFile(browser.toJSON()));
+};
 
 const downloadJSONFile = (json) => {
-    const path = 'igvjs.json'
-    const dataStr = JSON.stringify(json)
-    const data = URL.createObjectURL(new Blob([dataStr], {type: "application/octet-stream"}))
-    FileUtils.download(path, data)
-}
+  const path = "igvjs.json";
+  const dataStr = JSON.stringify(json);
+  const data = URL.createObjectURL(
+    new Blob([dataStr], { type: "application/octet-stream" })
+  );
+  FileUtils.download(path, data);
+};
 
-export default SaveSessionButton
+export default SaveSessionButton;
