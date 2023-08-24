e48498a156f258e26c398830d8d4a79bcb76ee49
fossilfriend <egreenfest@gmail.com>
2023-08-24 15:51:32 +0000

    exports

--- js/index.js
+++ js/index.js
@@ -1,41 +1,82 @@
 // Defines the top-level API for the igv module
+import MenuUtils from "./ui/menuUtils.js";
+import DataRangeDialog from "./ui/dataRangeDialog.js";
+import IGVGraphics from "./igv-canvas.js";
+import { createCheckbox } from "./igv-icons.js";
+import {
+  createBrowser,
+  createTrack,
+  removeAllBrowsers,
+  removeBrowser,
+  visibilityChange,
+} from "./igv-create.js";
+import { doAutoscale } from "./util/igvUtils.js";
+import embedCss from "./embedCss.js";
+import version from "./version.js";
+import TrackView from "./trackView.js";
+import {
+  igvxhr,
+  oauth,
+  IGVMath,
+  FeatureCache,
+  FileUtils,
+  IGVColor,
+  StringUtils
+} from "../node_modules/igv-utils/src/index.js";
 
-import MenuUtils from "./ui/menuUtils.js"
-import DataRangeDialog from "./ui/dataRangeDialog.js"
-import IGVGraphics from "./igv-canvas.js"
-import {createBrowser, createTrack, removeAllBrowsers, removeBrowser, visibilityChange} from './igv-create.js'
-import {doAutoscale} from "./util/igvUtils.js"
-import embedCss from "./embedCss.js"
-import version from "./version.js"
-import TrackView from "./trackView.js"
-import GenomeUtils from "./genome/genome.js"
-import {igvxhr, oauth} from "../node_modules/igv-utils/src/index.js"
+import GenomicInterval from "./genome/genomicInterval";
+import featurePacker from "./feature/featurePacker";
+import FeatureSource from "./feature/featureSource";
+import TrackBase from "../js/trackBase.js";
+import {BinnedColorScale, ConstantColorScale, GradientColorScale} from "./util/colorScale.js"
+import {randomColor as randomColorPalette, ColorTable, PaletteColorTable, appleCrayonPalette} from "./util/colorPalletes";
+import { makeVCFChords} from "./jbrowse/circularViewUtils";
 
-const setApiKey = igvxhr.setApiKey
+const setApiKey = igvxhr.setApiKey;
 
-embedCss()
+embedCss();
 
 function setGoogleOauthToken(accessToken) {
-    return oauth.setToken(accessToken)
+  return oauth.setToken(accessToken);
 }
 
 function setOauthToken(accessToken, host) {
-    return oauth.setToken(accessToken, host)
+  return oauth.setToken(accessToken, host);
 }
 
 export default {
-    IGVGraphics,
-    MenuUtils,
-    DataRangeDialog,
-    createTrack,
-    createBrowser,
-    removeBrowser,
-    removeAllBrowsers,
-    visibilityChange,
-    setGoogleOauthToken,
-    setOauthToken,
-    oauth,
-    version,
-    setApiKey
-}
-
+  IGVGraphics,
+  MenuUtils,
+  DataRangeDialog,
+  createTrack,
+  createBrowser,
+  removeBrowser,
+  removeAllBrowsers,
+  visibilityChange,
+  setGoogleOauthToken,
+  setOauthToken,
+  oauth,
+  version,
+  setApiKey,
+  doAutoscale,
+  TrackView,
+  featurePacker,
+  igvxhr,
+  FeatureCache,
+  IGVMath,
+  GenomicInterval,
+  TrackBase,
+  BinnedColorScale,
+  ConstantColorScale,
+  GradientColorScale,
+  randomColorPalette,
+  ColorTable, 
+  PaletteColorTable,
+  makeVCFChords,
+  FeatureSource,
+  FileUtils,
+  IGVColor,
+  StringUtils,
+  createCheckbox,
+  appleCrayonPalette
+};
