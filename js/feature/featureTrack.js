8ce35375156e81eb3f3a08b190d57633aed06d28
fossilfriend <egreenfest@gmail.com>
2023-08-30 17:39:57 +0000

    remove colon

--- js/feature/featureTrack.js
+++ js/feature/featureTrack.js
@@ -287,8 +287,21 @@ class FeatureTrack extends TrackBase {
                 // to catch name properties in both custom popupData functions and the generic extractPopupData function
 
                 const infoURL = this.infoURL || this.config.infoURL
+                const isGFF = "gff" === this.config.format || "gff3" === this.config.format || "gtf" === this.config.format
+
                 for (let fd of featureData) {
-                    data.push(fd)
+                    if (isGFF && this.colorBy) {
+                        if (fd.name.replace(':', '') === this.colorBy) {
+                            color = this.color ? this.color(fd.value) : this.colorTable ? this.colorTable.getColor(fd.value) : null;
+                            if (color !== null) {
+                                fd = Object.assign(fd, {color: color});
+                            }
+                        }
+                        data.push(fd);
+                    }
+                    else {
+                        data.push(fd);
+                    }
                     if (infoURL &&
                         fd.name &&
                         fd.name.toLowerCase() === "name" &&
@@ -305,7 +318,7 @@ class FeatureTrack extends TrackBase {
 
                 // If we have clicked over an exon number it.
                 // Disabled for GFF and GTF files if the visibility window is < the feature length since we don't know if we have all exons
-                const isGFF = "gff" === this.config.format || "gff3" === this.config.format || "gtf" === this.config.format
+                // const isGFF = "gff" === this.config.format || "gff3" === this.config.format || "gtf" === this.config.format
                 if (f.exons) {
                     for (let i = 0; i < f.exons.length; i++) {
                         const exon = f.exons[i]
